06fdc9cc2e544de31cb5963993d52b26052ea67b
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-01 15:31:46 -0400

    added target group

--- jenkins/ALB.tf
+++ jenkins/ALB.tf
@@ -19,6 +19,13 @@ resource "aws_lb" "ALB" {
 
 }
 
+resource "aws_lb_target_group" "test" {
+  name     = "jenkins-TG"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
 # resource "aws_lb_target_group_attachment" "test" {
 #   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:853931821519:targetgroup/jenkins-TG/4f738e491a3446aa"
 #   target_id        = [aws_instance.jenkins.id]
