f64a4949faa3a06b4373380cbba3bc0d30c126b3
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-03 10:31:18 -0400

    added vault

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -68,21 +68,6 @@ resource "aws_instance" "builder" {
 #   }
 # }
 
-resource "aws_instance" "vault" {
-  ami = "ami-007855ac798b5175e"
-
-  instance_type = "t2.medium"
-  key_name = "bastion"
-  vpc_security_group_ids = [aws_security_group.JSG.id]
-  subnet_id = "${element(module.vpc.private_subnets, 0)}"
-
-  tags = {
-    Name = "vault"
-    Department = "DevSecOps Associate" 
-    Creation = "terraform"
-    project = "interns"
-  }
-}
 
 
 
