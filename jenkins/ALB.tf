1de1707498c84c725af6ebfd7ba962ee3007d2ae
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-03 10:41:22 -0400

    added vault security group rules

--- jenkins/ALB.tf
+++ jenkins/ALB.tf
@@ -27,6 +27,13 @@ resource "aws_lb_target_group" "test" {
   vpc_id   = module.vpc.vpc_id
 }
 
+resource "aws_lb_target_group" "vault" {
+  name     = "vault-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
 # resource "aws_lb_target_group_attachment" "test" {
 #   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:853931821519:targetgroup/jenkins-TG/4f738e491a3446aa"
 #   target_id        = [aws_instance.jenkins.id]
--- jenkins/security-group.tf
+++ jenkins/security-group.tf
@@ -48,6 +48,14 @@ resource "aws_security_group" "JSG" {
     security_groups = [aws_security_group.ALB-SG.id]
   }
 
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    security_groups = [aws_security_group.ALB-SG.id]
+  }
+
    egress {
     from_port        = 0
     to_port          = 0
@@ -70,6 +78,14 @@ resource "aws_security_group" "JSG" {
   ingress {
     from_port = 80
     to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 81
+    to_port   = 81
     protocol  = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
