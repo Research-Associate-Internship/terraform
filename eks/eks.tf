22946018298e6daef121e2d7c69f3ef3d809ad69
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-03 10:16:23 -0400

    added eks terraform

---
+++ eks/eks.tf
@@ -0,0 +1,43 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.10.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.25"
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = false
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+
+}
+
+module "fargate_profile" {
+  source = "terraform-aws-modules/eks/aws//modules/fargate-profile"
+
+  name         = "FG-test"
+  cluster_name = "my-cluster"
+
+  subnet_ids = module.vpc.private_subnets
+  selectors = [{
+    namespace = "kube-system"
+  }]
+
+  tags = {
+    Department = "DevSecOps Associate"
+    project = "interns"
+    Creation = "terraform"
+  }
+}
