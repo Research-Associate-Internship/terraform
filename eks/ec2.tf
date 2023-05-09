8f02fd04c0075a7c68b35aa6d96eae06c98c2bb0
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-09 15:15:38 -0400

    added bastion

---
+++ eks/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "eks-bastion" {
+  ami = "ami-007855ac798b5175e"
+
+  instance_type = "t2.medium"
+  key_name = "bastion"
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
+  subnet_id = module.vpc.public_subnets[0]
+
+  tags = {
+    Name = "eks-bastion"
+    Department = "DevSecOps Associate" 
+    Creation = "terraform"
+    project = "interns"
+  }
+}
---
+++ eks/security-group.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "bastion-sg" {
+  name = "eks-bastion-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "71.163.48.190/32", "209.183.243.114/32", "73.213.124.24/32"
+    ]
+  }
+
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-bastion-sg"
+    Department = "DevSecOps Associate" 
+    Creation = "terraform"
+  }
+
+}
