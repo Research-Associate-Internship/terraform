7090dcb2d4c468f264f1a4fdacf6a6b7419b94a5
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-09 11:27:39 -0400

    added back eks.tf

---
+++ eks/eks.tf
@@ -0,0 +1,32 @@
+resource "aws_eks_cluster" "devsecops {
+  name     = "devsecops-cluster"
+  role_arn = aws_iam_role.devsecops-cluster.arn
+
+  vpc_config {
+    subnet_ids = [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+  }
+}
+
+resource "aws_iam_role" "devsecops-cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "betty-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.devsecops-cluster.name
+}
--- testeks.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_eks_cluster" "devsecops {
-  name     = "betty-cluster"
-  role_arn = aws_iam_role.devsecops.arn
-
-  vpc_config {
-    subnet_ids = [module.vpc.public_subnets[0], ]
-  }
-
-}
