5926f24076d7dcdc7a14b8c0025e998e9d10a91a
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-10 18:53:13 -0400

    added bastion security group to node

--- eks/ng.tf
+++ eks/ng.tf
@@ -16,7 +16,7 @@ resource "aws_eks_node_group" "NextGenDS-node-group" {
 
   remote_access {
     ec2_ssh_key               = "bastion"
-    source_security_group_ids = [aws_security_group.node-group-sg.id]
+    source_security_group_ids = [aws_security_group.node-group-sg.id, aws_security_group.bastion-sg.id]
   }
 
   depends_on = [
