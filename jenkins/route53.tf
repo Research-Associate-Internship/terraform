62b72f3461f99f41b12fada8233dd4e35fe504fc
hchoi3 <hyunmin.choi@axleinfo.com>
2023-05-11 14:31:39 -0400

    corrected route 53 changin ALB arn to dns_name

--- jenkins/route53.tf
+++ jenkins/route53.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_record" "vault" {
   zone_id = "Z03309881PN2JZ6GGLOJ8"
-  name    = "vault-aws"
+  name    = "vault-aws.axle-interns.com"
   type    = "A"
 
   alias {
-    name                   = aws_lb.vault.arn
+    name                   = aws_lb.vault.dns_name
     zone_id                = "Z35SXDOTRQ7X7K"
     evaluate_target_health = true
   }
