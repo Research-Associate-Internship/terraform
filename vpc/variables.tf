2a384dcaadd1c9e7525e415136c746b194005d3e
RJ Padilla <rjpadilla.dev@outlook.com>
2023-07-28 15:06:42 -0400

    feat: add VPC module (#15)
    * created VPC module
    
    * Add main.tf file
    
    * added missing quote
    
    * Changed subnets to public and private
    
    * Added private subnets, gateways, and associations

---
+++ vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_cidr" {}
+variable "public_subnet_1_cidr" {}
+variable "public_subnet_2_cidr" {}
+variable "private_subnet_1_cidr" {}
+variable "private_subnet_2_cidr" {}
+variable "private_subnet_3_cidr" {}
+variable "private_subnet_4_cidr" {}
