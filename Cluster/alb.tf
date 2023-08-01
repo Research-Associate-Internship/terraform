8b4312009958796f5e5538e41eb3f98d064860ff
Dinesh Bhogadi <bhogadi2022@gmail.com>
2023-08-01 09:45:29 -0500

    Updating VPC id variable in ALB.tf

--- Cluster/alb.tf
+++ Cluster/alb.tf
@@ -46,7 +46,7 @@ resource "aws_lb_target_group" "NextGenDS_target_group" {
   name               = "NextGenDS_target_group"
   port               = 80
   protocol           = "HTTP"
-  vpc_id             = module.vpc.Vpc_id
+  vpc_id             = var.Vpc_id
   health_check{
     healthy_threshold =var.health_check["health_threshold"]
     internal          =var.health_check["interval"]
@@ -71,7 +71,7 @@ resource "aws_lb_target_group_attachment" "NextGenDS_target_group-attach" {
 
 resource "aws_security_group" "NextGenDS-sg" {
   name   = "Rac2-NextGenDS-alb-sg"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = var.Vpc_id
 
   ingress {
     from_port = 80
