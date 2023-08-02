5fc3c4677e712627a7f6f7118c731666020ff8e5
Mukund-S <mukundr18@gmail.com>
2023-08-02 09:21:34 -0400

    Added Bastion Host

---
+++ Bastion/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+}
+
+variable "bastion_sg_id" {
+    description = "Bastion Host Security Group ID"
+}
+
+variable "public_subnet_2_id" {
+    description = "public subnet id"
+}
