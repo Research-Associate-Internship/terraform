3a7cf18f067736619d84d3d52ca7ac798600723d
RJ Padilla <rjpadilla.dev@outlook.com>
2023-08-02 10:14:33 -0400

    modularized ecr (#22)

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
+module "ecr" {
+    source = "./ecr"
+    erc_name = "rac2-devsecops"
+}
+
 module "vpc" {
     source = "./vpc"
     vpc_cidr = "10.0.0.0/24"
