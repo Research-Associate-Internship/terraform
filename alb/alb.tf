44e1284e06a540915b8fe12a342b8c1beeb64ee6
Mukund-S <mukundr18@gmail.com>
2023-08-09 12:17:00 -0400

    Testing

--- alb/alb.tf
+++ alb/alb.tf
@@ -1,98 +1,113 @@
 resource "aws_lb" "NextGenDS" {
-  name               = "Rac2-NextGenDS-ALB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.NextGenDS-alb-sg.id]
-  subnets            = [var.private_subnets[0],var.private_subnets[1]]
-  enable_cross_zone_load_balancing ="true"
-
+  name                             = "Rac2-NextGenDS-ALB"
+  internal                         = false
+  load_balancer_type               = "application"
+  security_groups                  = [aws_security_group.NextGenDS-alb-sg.id]
+  subnets                          = [var.public_subnets[0], var.public_subnets[1]]
+  enable_cross_zone_load_balancing = "true"
   tags = {
-    Department       = "DevOps RAC2"
-    Creation         = "terraform"
-    Project          = "intern"
-    Envirornment     = "Production"
+    Department   = "DevOps RAC2"
+    Creation     = "terraform"
+    Project      = "intern"
+    Envirornment = "Production"
   }
 }
-
 resource "aws_lb_listener" "NextGenDS-http-listener" {
-  load_balancer_arn  = aws_lb.NextGenDS.arn
-  port               = "80"
-  protocol           = "HTTP"
-
+  load_balancer_arn = aws_lb.NextGenDS.arn
+  port              = "80"
+  protocol          = "HTTP"
   default_action {
     type = "redirect"
     redirect {
-      port           = "443"
-      protocol       = "HTTPS"
-      status_code    = "HTTP_301"
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
-
 resource "aws_lb_listener" "NextGenDS-https-listener" {
-  load_balancer_arn  = aws_lb.NextGenDS.arn
-  port               = "443"
-  protocol           = "HTTPS"
-  certificate_arn    = var.cerificate_arn
-
+  load_balancer_arn = aws_lb.NextGenDS.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = var.cerificate_arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.NextGenDS-target-group.arn
   }
 }
-
 resource "aws_lb_target_group" "NextGenDS-target-group" {
-  name               = "NextGenDS-target-group"
-  port               = 80
-  protocol           = "HTTP"
-  vpc_id             = var.Vpc_id
+  name     = "NextGenDS-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.Vpc_id
+  health_check {
+    path = "/health"
+  }
   tags = {
-    Department       = "DevOps RAC2"
-    Creation         = "terraform"
-    Project          = "intern"
-    Envirornment     = "Production"
+    Department   = "DevOps RAC2"
+    Creation     = "terraform"
+    Project      = "intern"
+    Envirornment = "Production"
   }
 }
-
 resource "aws_lb_target_group_attachment" "NextGenDS_target-group-attach" {
-  target_group_arn  = aws_lb_target_group.NextGenDS-target-group.arn
-  target_id         = var.eks_node_ids[0]
-  port              = 80
+  target_group_arn = aws_lb_target_group.NextGenDS-target-group.arn
+  target_id        = var.eks_node_ids[0]
+  port             = 80
 }
-
 resource "aws_security_group" "NextGenDS-alb-sg" {
   name   = "NextGenDS-alb-sg"
   vpc_id = var.Vpc_id
-
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
-    Department       = "DevOps RAC2"
-    Creation         = "terraform"
-    Project          = "intern"
-    Envirornment     = "Production"
+    Department   = "DevOps RAC2"
+    Creation     = "terraform"
+    Project      = "intern"
+    Envirornment = "Production"
   }
-
+}
+resource "aws_security_group" "NextGenDS-target-sg" {
+  name   = "NextGenDS-target-sg"
+  vpc_id = var.Vpc_id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Department   = "DevOps RAC2"
+    Creation     = "terraform"
+    Project      = "intern"
+    Envirornment = "Production"
+  }
 }
--- alb/variables.tf
+++ alb/variables.tf
@@ -1,26 +1,26 @@
 variable "private_subnets" {
-    type               = list(string)
-    description        = "List of private subnets"
+  type        = list(string)
+  description = "List of private subnets"
 }
 
 variable "public_subnets" {
-    type               = list(string)
-    description        = "List of public subnets"
+  type        = list(string)
+  description = "List of public subnets"
 
 }
 
-variable "Vpc_id"{
-    type               = string
-    description        = "List of availability zones"
+variable "Vpc_id" {
+  type        = string
+  description = "List of availability zones"
 
 }
 
 variable "cerificate_arn" {
-  type               = string
+  type        = string
   description = "Certificte ARN for HTTPS"
   default     = "arn:aws:acm:us-east-1:853931821519:certificate/3d7723cf-2431-41a4-8fc5-a60dd3fa02fa"
 }
 
 variable "eks_node_ids" {
-  type               = list(string)
+  type = list(string)
 }
