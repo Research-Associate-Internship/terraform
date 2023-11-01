6465856b571291de5c885437e993204885839056
sai kasaragadda <saikasaragadda222@gmail.com>
2023-10-31 23:04:08 -0400

    node group created

--- eks/ng.tf
+++ eks/ng.tf
@@ -1,76 +1,76 @@
-# resource "aws_eks_node_group" "NextGenDS-rac3-node-group" {
-#   cluster_name    = aws_eks_cluster.NextGenDS-rac3.name
-#   node_group_name = "NextGenDS-rac3-node-group"
-#   node_role_arn   = aws_iam_role.NextGenDS-role.arn
-#   subnet_ids      = [module.vpc.private_subnets[0]] //, module.vpc.private_subnets[1]]
+resource "aws_eks_node_group" "NextGenDS-rac3-node-group" {
+  cluster_name    = aws_eks_cluster.NextGenDS-rac3.name
+  node_group_name = "NextGenDS-rac3-node-group"
+  node_role_arn   = aws_iam_role.NextGenDS-role.arn
+  subnet_ids      = ["subnet-087b37379578cd037","subnet-0aa9f30d1300a6a64"] //, module.vpc.private_subnets[1]]
 
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 1
-#   }
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
 
-#   update_config {
-#     max_unavailable = 1
-#   }
+  update_config {
+    max_unavailable = 1
+  }
 
-#   remote_access {
-#     ec2_ssh_key               = "bastion"
-#     source_security_group_ids = [aws_security_group.bastion-sg.id]
-#   }
+  remote_access {
+    ec2_ssh_key               = "bastion"
+    source_security_group_ids = [aws_security_group.bastion-sg.id]
+  }
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.NextGenDS-AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.NextGenDS-AmazonEKS_CNI_Policy,
-#     aws_iam_role_policy_attachment.NextGenDS-AmazonEC2ContainerRegistryReadOnly,
-#     aws_iam_role_policy_attachment.NextGenDS-CloudWatchLogsFullAccess,
-#   ]
+  depends_on = [
+    aws_iam_role_policy_attachment.NextGenDS-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.NextGenDS-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.NextGenDS-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.NextGenDS-CloudWatchLogsFullAccess,
+  ]
 
-#   ami_type       = "AL2_x86_64"
-#   instance_types = ["c4.2xlarge"]
-#   #disk_size = "50"
-#   tags = {
-#     Department = "DevSecOps Associate"
-#     project    = "interns"
-#     Creation   = "terraform"
-#   }
+  ami_type       = "AL2_x86_64"
+  instance_types = ["c4.2xlarge"]
+  #disk_size = "50"
+  tags = {
+    Department = "DevSecOps Associate"
+    project    = "interns"
+    Creation   = "terraform"
+  }
 
-# }
+}
 
-# resource "aws_iam_role" "NextGenDS-role" {
-#   name = "nodegroup-role"
+resource "aws_iam_role" "NextGenDS-role" {
+  name = "nodegroup-role1"
 
-#   assume_role_policy = jsonencode({
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       }
-#     }]
-#     Version = "2012-10-17"
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "NextGenDS-AmazonEKS_CNI_Policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.NextGenDS-role.name
-# }
+resource "aws_iam_role_policy_attachment" "NextGenDS-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.NextGenDS-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "NextGenDS-AmazonEKSWorkerNodePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role       = aws_iam_role.NextGenDS-role.name
-# }
+resource "aws_iam_role_policy_attachment" "NextGenDS-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.NextGenDS-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "NextGenDS-AmazonEC2ContainerRegistryReadOnly" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.NextGenDS-role.name
-# }
+resource "aws_iam_role_policy_attachment" "NextGenDS-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.NextGenDS-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "NextGenDS-CloudWatchLogsFullAccess" {
-#  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-#  role    = aws_iam_role.NextGenDS-role.name
-# }
+resource "aws_iam_role_policy_attachment" "NextGenDS-CloudWatchLogsFullAccess" {
+ policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+ role    = aws_iam_role.NextGenDS-role.name
+}
 
 # resource "aws_eks_node_group" "NextGenDS-node-group2" {
 #   cluster_name    = aws_eks_cluster.NextGenDS.name
