c8185eb40a1e05fee0e7bf42f105e0f26a2ab434
SaiKasaragadda1 <119982694+SaiKasaragadda1@users.noreply.github.com>
2023-11-03 21:04:39 -0400

    Update route53.tf

--- eks/route53.tf
+++ eks/route53.tf
@@ -1,15 +1,15 @@
-# resource "aws_route53_record" "NextGenDS-rac3" {
-#   zone_id = "Z03309881PN2JZ6GGLOJ8"
-#   name    = "NextGenDS-rac3.axle-interns.com"
-#   type    = "A"
+ resource "aws_route53_record" "NextGenDS-rac3" {
+   zone_id = "Z03309881PN2JZ6GGLOJ8"
+   name    = "NextGenDS-rac3.axle-interns.com"
+   type    = "A"
 
-#   alias {
-#     name                   = "a5e4e91e7edb645da9c9e56969ba1572-1354975883.us-east-1.elb.amazonaws.com"
-#     zone_id                = "Z35SXDOTRQ7X7K"
-#     evaluate_target_health = true
-#   }
+   alias {
+     name                   = "a974ca6fa09694380a82f1e11c1b3730-1473608677.us-east-1.elb.amazonaws.com"
+     zone_id                = "Z35SXDOTRQ7X7K"
+     evaluate_target_health = true
+   }
 
-# }
+ }
 
 # resource "aws_route53_record" "kibana" {
 #   zone_id = "Z03309881PN2JZ6GGLOJ8"
