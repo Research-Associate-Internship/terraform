c38f113688b7829da4c2bc97a51146c2d81b9d5b
SaiKasaragadda1 <119982694+SaiKasaragadda1@users.noreply.github.com>
2023-11-13 12:54:18 -0500

    Update route53.tf

--- eks/route53.tf
+++ eks/route53.tf
@@ -1,12 +1,25 @@
  resource "aws_route53_record" "NextGenDS-rac3" {
    zone_id = "Z03309881PN2JZ6GGLOJ8"
-   name    = "nextgends-rac3.axle-interns.com"
+   name    = "vote-nextgends-rac3.axle-interns.com"
    type    = "A"
 
    alias {
-     name                   = "k8s-game2048-ingress2-46ee75eeb1-403890270.us-east-1.elb.amazonaws.com"
+     name                   = "a883535d12a3f4ef79961d0ad5969e6c-1948833605.us-east-1.elb.amazonaws.com"
      zone_id                = "Z35SXDOTRQ7X7K"
      evaluate_target_health = true
    }
 
  }
+resource "aws_route53_record" "NextGenDS-rac3" {
+   zone_id = "Z03309881PN2JZ6GGLOJ8"
+   name    = "result-nextgends-rac3.axle-interns.com"
+   type    = "A"
+
+   alias {
+     name                   = "a883535d12a3f4ef79961d0ad5969e6c-1948833605.us-east-1.elb.amazonaws.com"
+     zone_id                = "Z35SXDOTRQ7X7K"
+     evaluate_target_health = true
+   }
+
+ }
+
