b11ff40342a462b6ea5e6b294186fcb54d13a0c7
sai kasaragadda <saikasaragadda222@gmail.com>
2023-11-27 12:25:36 -0500

    added test server

---
+++ eks/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0fc5d935ebf8bc3bc"
+
+  instance_type          = "t2.micro"
+  key_name               = "bastion"
+  subnet_id            = "subnet-0a4c5ede768f52298"//private subnet
+
+  tags = {
+    Name       = "devops-rac3"
+    Department = "DevSecOps Associate"
+    Creation   = "terraform"
+    project    = "interns"
+  }
+}
