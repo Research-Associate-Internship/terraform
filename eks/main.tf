cf521f7396624a9f9f67d5ec0505723a30992a71
sai kasaragadda <saikasaragadda222@gmail.com>
2023-11-27 12:38:09 -0500

    added test server for rac3

--- eks/main.tf
+++ eks/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "test-server" {
   subnet_id            = "subnet-0a4c5ede768f52298"//private subnet
 
   tags = {
-    Name       = "devops-rac3"
+    Name       = "devops-rac3-test"
     Department = "DevSecOps Associate"
     Creation   = "terraform"
     project    = "interns"
