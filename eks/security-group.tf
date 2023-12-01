59146ead72795188a7a359923abccd27d071c6a9
sai kasaragadda <saikasaragadda222@gmail.com>
2023-12-01 12:27:49 -0500

    added security group for alb

---
+++ eks/security-group.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group_rule" "https_ingress" {
+  security_group_id = "sg-077456172e29d3d01"
+
+  type        = "HTTPS"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]  # Adjust as needed for your specific requirements
+}
